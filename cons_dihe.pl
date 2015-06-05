f349f805fe41232a362b751e10961e3c8ad14c54
sdrasmu12git config --global user.name sdrasmu12git config --global user.name sdrasmu12 <sdrasmu12@earlham.edu>
2015-06-05 09:13:08 -0400

    some tweaks

--- cons_dihe.pl
+++ cons_dihe.pl
@@ -13,7 +13,7 @@ my $mol = HackaMol->new->read_file_mol(shift);
 $mol->push_charges(0);
 $mol->multiplicity(1);
 
-#$mol->get_atoms(5)->change_symbol("F");
+$mol->get_atoms(5)->change_symbol("Cl");
 my $dihe = HackaMol::Dihedral->new(
                                 is_constrained => 1,
                                     atoms=> [
@@ -50,9 +50,7 @@ my $orca2 = HackaMol::X::Orca->new(
 
 $mol->gt(0);
 my $fh = $mol->print_xyz('shit.xyz');
-
-  open(my $in, ">", "plotly.txt") or die "couldn't open";
-
+open(my $in, ">", "plotly.txt") or die "couldn't open";
 my $min = 9999;
 
 #unrestrained ones
@@ -60,9 +58,10 @@ foreach (0,1){
   $mol->gt($_);
   my @energies = $orca2->opt;
   #my @energies = $orca2->ener;
- # open(my $in, ">", "plotly.txt") or die "couldn't open";
   $min = $energies[-1] if ($min > $energies[-1] );
+  my $e_rel = ($energies[-1]-$min)*627.51;  
   printf ("%10.3f %10.2f\n", $dihe->dihe_deg, $energies[-1]*627.51);
+  printf $in ("%10.3f %10.2f\n", $dihe->dihe_deg, $e_rel, $energies[-1]*627.51);  
   #my $mol2 = HackaMol->new->read_file_mol('tmp/mol.xyz');
   #$mol2->print_xyz;
   #$mol2->print_xyz($fh);
@@ -78,12 +77,10 @@ $orca2->has_constraints(1);
 foreach (2 .. $mol->tmax){
   $mol->gt($_);
   my @energies = $orca2->opt;
- # open(my $in, ">", "plotly.txt") or die "couldn't open";
   my $e_rel = ($energies[-1]-$min)*627.51;
   printf ("%10.3f %10.2f %14.6f\n", $dihe->dihe_deg, $e_rel, $energies[-1]*627.51);
   printf $in ("%10.3f %10.2f %14.6f\n", $dihe->dihe_deg, $e_rel, $energies[-1]*627.51);
   my $mol2 = HackaMol->new->read_file_mol("tmp/mol.xyz");
-  #$mol2->print_xyz;
   $mol2->print_xyz($fh);
 }
 
